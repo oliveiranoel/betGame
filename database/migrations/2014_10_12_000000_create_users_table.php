f540f59eb66bb838139d13765a1fb6268ed03146
oliveiranoel <45029331+oliveiranoel@users.noreply.github.com>
2020-04-18 14:30:26 +0200

    controllers and admin role for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('points')->default(0);
+            $table->boolean('admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
