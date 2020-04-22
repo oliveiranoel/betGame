b728cde173c4de8a10a1e03c2d31d0ff97dcb259
Dominik Suter <dominik.suter@bbzsogr.ch>
2020-04-22 21:07:55 +0200

    nav admin and user

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -37,12 +37,15 @@
                         <a class="dropdown-item" href="{{ route('bet.index') }}">
                             {{ __('Bet') }}
                         </a>
+                        @if ( Auth::user()->admin )
+                        <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="{{ route('team.index') }}">
                             {{ __('Team') }}
                         </a>
                         <a class="dropdown-item" href="{{ route('game.index') }}">
                             {{ __('Game') }}
                         </a>
+                        @endif
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="{{ route('logout') }}"
                            onclick="event.preventDefault();
